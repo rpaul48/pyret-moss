e07605ae5516a6d2028119149c1d8a36e18f70b7
Raj Paul <rajdpaul01@gmail.com>
2020-07-28 14:41:40 -0400

    set up new Rust project

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+//use std::io;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    // ask user for input directory of files
+    //println!("Please enter the path to a directory of files:");
+
+    //let mut folder_path_str = String::new();
+    //io::stdin()
+    //    .read_line(&mut folder_path_str)
+    //    .expect("Failed to read input");
+
+    //let str1 = &folder_path_str[..];
+    //let folder_path = Path::new(str1);
+
+    let folder_path = Path::new("./test-dirs/txts");
+
+    for file in fs::read_dir(folder_path).unwrap() {
+        println!("file path: {}", file.unwrap().path().display())
+    }
+}
