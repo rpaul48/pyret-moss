4ccfedf59e98205bbc7651af0c30faf661ad4fd2
Raj Paul <rajdpaul01@gmail.com>
2020-08-09 13:16:40 -0400

    wrote pseudocode outline for phase_ii

--- src/phase_ii.rs
+++ src/phase_ii.rs
@@ -5,7 +5,29 @@ use crate::{Doc, Sub};
 
 // Consider pairs of submissions that overlap, associate them with
 // the fingerprints they share, and order pairs by the quantity shared.
-fn find_overlaps(hash_to_subs: FnvHashMap<i64, HashSet<&Sub>>) 
+fn find_overlaps(hash_to_subs: FnvHashMap<i64, HashSet<&Sub>>)
     -> Vec<(HashSet<&Sub>, HashSet<i64>)> {
+
+        // let pairs_to_hashes: HashMap<HashSet<&Sub>, HashSet<i64>> = HashMap::new();
+
+
+        // iterate through hash_to_subs by key
+
+            // if the current key K has a value V with len > 1
+
+                // for each pair P of subs in V, add K to the HashSet<i64> that P
+                // maps to in pairs_to_hashes
+
+
+        // let sorted_kv_tuples: Vec<(HashSet<&Sub>, HashSet<i64>)> = Vec::new();
+
+        // iterate through pairs_to_hashes by key
+
+            // insert the current KV pair into sorted_kv_tuples such that the Vec is always in
+            // order of descending value length
+
+
+        // sorted_kv_tuples
+
     unimplemented!();
 }
