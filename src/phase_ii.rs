741818373908e32e220ec72e1963a73037624e1b
Raj Paul <rajdpaul01@gmail.com>
2020-08-09 22:24:29 -0400

    finished writing phase_ii function, switched to BTreeSets as keys in map

--- src/phase_ii.rs
+++ src/phase_ii.rs
@@ -1,33 +1,60 @@
 
 use std::collections::HashSet;
+use std::collections::HashMap;
+use std::collections::BTreeSet;
 use fnv::FnvHashMap;
+use std::iter::FromIterator;
 use crate::{Doc, Sub};
 
 // Consider pairs of submissions that overlap, associate them with
 // the fingerprints they share, and order pairs by the quantity shared.
 fn find_overlaps(hash_to_subs: FnvHashMap<i64, HashSet<&Sub>>)
-    -> Vec<(HashSet<&Sub>, HashSet<i64>)> {
+    -> Vec<(BTreeSet<&Sub>, HashSet<i64>)> {
 
-        // let pairs_to_hashes: HashMap<HashSet<&Sub>, HashSet<i64>> = HashMap::new();
-
+        // a map whose keys are pairs (sets of size 2) of subs and whose values are sets of hashes
+        let mut pairs_to_hashes: HashMap<BTreeSet<&Sub>, HashSet<i64>> = HashMap::new();
 
         // iterate through hash_to_subs by key
+        for (hash, subs) in hash_to_subs {
+            // if the current key has a value containing more than one entry
+            let subs_len: usize = subs.len();
+            if subs_len > 1 {
 
-            // if the current key K has a value V with len > 1
+                // get all possible pairs of submissions within subs
+                let ordered_subs: Vec<&&Sub> = Vec::from_iter(subs.iter());
+                let mut i: usize = 0;
 
-                // for each pair P of subs in V, add K to the HashSet<i64> that P
-                // maps to in pairs_to_hashes
+                while i < (subs_len - 1) {
+                    let mut j: usize = i + 1;
+                    while j < (subs_len) {
+                        // the current pair of submissions
+                        let mut sub_pair: BTreeSet<&Sub> = BTreeSet::new();
+                        sub_pair.insert(*ordered_subs[i]);
+                        sub_pair.insert(*ordered_subs[j]);
 
+                            // if sub_pair is not already a key in pairs_to_hashes, add it and
+                            // make it map to a set containing hash; otherwise, add hash to the
+                            // set sub_pair already maps to
+                            pairs_to_hashes.entry(sub_pair)
+                                .or_insert_with(HashSet::new)
+                                .insert(hash);
+                        j += 1;
+                    }
+                    i += 1;
+                }
+            }
+        }
 
-        // let sorted_kv_tuples: Vec<(HashSet<&Sub>, HashSet<i64>)> = Vec::new();
+        // iterate through pairs_to_hashes, add (key, value) tuples to the pair_hash_tuples Vec
+        let mut pair_hash_tuples: Vec<(BTreeSet<&Sub>, HashSet<i64>)> = Vec::new();
 
-        // iterate through pairs_to_hashes by key
+        for (sub_pair, matching_hashes) in pairs_to_hashes {
+            pair_hash_tuples.push((sub_pair, matching_hashes));
+        }
 
-            // insert the current KV pair into sorted_kv_tuples such that the Vec is always in
-            // order of descending value length
+        // sort the pair_hash_tuples vec by descending number of matches
+        pair_hash_tuples.sort_by(|a, b| a.1.len().cmp(&b.1.len()));
 
-
-        // sorted_kv_tuples
-
-    unimplemented!();
+        // return the populated, sorted output
+        pair_hash_tuples
 }
