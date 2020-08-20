fb259233a0acd3fd8892ee5679524bbfb74dc681
Thomas Castleman <thomas7rex@gmail.com>
2020-08-20 17:21:59 -0400

    implement choosing of common substrings

--- src/phase_iii.rs
+++ src/phase_iii.rs
@@ -1,6 +1,6 @@
 /* Phase III: Find common substrings of fingerprints in a submission pair */
 
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap, BTreeMap};
 use std::cmp::{min, max};
 use crate::phase_ii::SubPair;
 use crate::fingerprint::Fingerprint;
@@ -113,7 +113,119 @@ fn substr_table(rows: &FpVec, cols: &FpVec) -> SubStrTable {
 // Choose longest common substrings from the substring table such that each row/col 
 // fingerprint has at least 1 of their longest common substrings in the chosen set
 fn choose_substrs(rows: &FpVec, cols: &FpVec, table: &SubStrTable) -> HashSet<SubString> {
-    unimplemented!();
+    let mut all_substrs = Vec::new();
+    let mut row_to_substrs: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+    let mut col_to_substrs: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+
+    let mut a_doc_idx = 0;
+
+    // for each row element
+    for (r, row_elt) in rows.iter().enumerate() {
+        // track A's document index by Nones in row vector
+        if row_elt.is_none() && r > 0 {
+            a_doc_idx += 1;
+            continue;
+        }
+
+        let mut b_doc_idx = 0;
+
+        // for each column element
+        for (c, col_elt) in cols.iter().enumerate() {
+            // track B's document index by Nones in column vector
+            if col_elt.is_none() && c > 0 {
+                b_doc_idx += 1;
+                continue;
+            }
+
+            // if not the start of a substring (1), proceed
+            if table[r][c] == 0 || table[r][c] > 1 { continue; }
+
+            // construct new substring by following diagonal
+            let new_substr = trace_diagonal(table, (rows, cols), (r, c), (a_doc_idx, b_doc_idx));
+
+            all_substrs.push(new_substr.clone());       // add copy to substrings vec
+            let new_substr_idx = all_substrs.len() - 1; // get index of newly-added substring
+            
+            let affected_rows = (r..(r + new_substr.size));
+            let affected_cols = (c..(c + new_substr.size));
+
+            // update affected rows/cols to include a ref to this substring
+            for row_idx in affected_rows {
+                row_to_substrs.entry(row_idx)
+                    .or_insert_with(Vec::new)
+                    .push(new_substr_idx);
+            }
+            for col_idx in affected_cols {
+                col_to_substrs.entry(col_idx)
+                    .or_insert_with(Vec::new)
+                    .push(new_substr_idx);
+            }
+        }
+    }
+
+    // choose substrings for row fingerprints, then column fingerprints
+    let chosen_for_rows = choose_for_dim(&row_to_substrs, &all_substrs, &HashSet::new());
+    let chosen_for_cols = choose_for_dim(&col_to_substrs, &all_substrs, &chosen_for_rows);
+
+    // combine all chosen indices into one set
+    let chosen_idxs = chosen_for_rows.union(&chosen_for_cols);
+
+    // transfer chosen substrings into a set, to return
+    let mut chosen_substrs = HashSet::new();
+    for &idx in chosen_idxs {
+        chosen_substrs.insert(all_substrs[idx].clone());
+    }
+
+    chosen_substrs
+}
+
+// Choose a set of substrings (by index) for the fingerprints along a given dimension
+// (row/col) such that at least one of each fp's longest common substring is included
+fn choose_for_dim(dim_to_substrs: &BTreeMap<usize, Vec<usize>>, 
+    all_substrs: &Vec<SubString>, chosen: &HashSet<usize>) -> HashSet<usize> {
+    // new substrings to add, having processed this dimension
+    let mut chosen_this_dim: HashSet<usize> = HashSet::new();
+
+    // for each fingerprint in this dimension
+    for (_, substr_idxs) in dim_to_substrs {
+        // (already_chosen, max_idx) indicates that all_substrs[max_idx] is the
+        // max length substring for this dimension, & whether or not it has been chosen already
+        let mut max: Option<(bool, usize)> = None;
+
+        // find the max substring that includes this fingerprint
+        for &idx in substr_idxs.iter() {
+            // it's chosen if it was chosen either for this or a previous dimension
+            let this_already_chosen = chosen.contains(&idx) || chosen_this_dim.contains(&idx);
+
+            match max {
+                // previous max exists
+                Some((max_already_chosen, max_idx)) => {
+                    let max_size = all_substrs[max_idx].size;
+                    let this_size = all_substrs[idx].size;
+
+                    // if this substring is longer, OR
+                    // if same size & this substr is already chosen but max is not
+                    if  (this_size > max_size) || 
+                        (!max_already_chosen && (this_size == max_size) && this_already_chosen) {
+                        max = Some((this_already_chosen, idx));
+                    }
+                },
+                // no previous max, use this
+                None => { max = Some((this_already_chosen, idx)); }
+            };
+        }
+
+        if let Some((already_chosen, idx)) = max {
+            // if max substring for this fp is *new*, then insert it as chosen
+            if !already_chosen {
+                chosen_this_dim.insert(idx);
+            }
+        }
+    }
+
+    // return set of substrs (indices) that were chosen for this dimension
+    // (and weren't already chosen)
+    chosen_this_dim
 }
 
 // Trace diagonally down/right from table[r][c] to construct a SubString 
@@ -546,11 +658,11 @@ mod tests {
                 hashes: vec![1, 2, 1],
                 a_entry: Entry {
                     doc_idx: 0,
-                    lines: (14, 22)
+                    lines: (1, 15)
                 },
                 b_entry: Entry {
                     doc_idx: 1,
-                    lines: (1, 15)
+                    lines: (14, 22)
                 }
             });
             exp.insert(SubString {
@@ -636,5 +748,53 @@ mod tests {
 
             assert_eq!(choose_substrs(&rows, &cols, &table), exp);
         }
+    }
+
+    #[test]
+    fn test_choose_for_dim() {
+        let all = vec![
+            SubString {
+                size: 3,
+                hashes: vec![41, 25, 18],
+                a_entry: Entry { doc_idx: 0, lines: (2, 15) },
+                b_entry: Entry { doc_idx: 0, lines: (14, 29) }
+            },
+            SubString {
+                size: 3,
+                hashes: vec![99, 26, 30],
+                a_entry: Entry { doc_idx: 0, lines: (44, 59) },
+                b_entry: Entry { doc_idx: 0, lines: (16, 20) }
+            },
+            SubString {
+                size: 2,
+                hashes: vec![7, 10],
+                a_entry: Entry { doc_idx: 0, lines: (89, 102) },
+                b_entry: Entry { doc_idx: 0, lines: (66, 81) }
+            }
+        ];
+
+        // choose for rows
+        let mut row_to_substrs: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+        row_to_substrs.insert(1, vec![2]);
+        row_to_substrs.insert(2, vec![1, 0, 2]);
+        row_to_substrs.insert(3, vec![0, 1, 2]);
+
+        let row_out = choose_for_dim(&row_to_substrs, &all, &HashSet::new());
+        let exp_row_out: HashSet<usize> = [2, 1].iter().cloned().collect();
+
+        assert_eq!(row_out, exp_row_out);
+
+        // choose for cols
+        let mut col_to_substrs: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+        col_to_substrs.insert(1, vec![2, 0]);
+        col_to_substrs.insert(2, vec![1, 2]);
+        col_to_substrs.insert(3, vec![2]);
+        col_to_substrs.insert(4, vec![0, 1, 2]);
+
+        // use row output as already chosen
+        let col_out = choose_for_dim(&col_to_substrs, &all, &row_out);
+        let exp_col_out: HashSet<usize> = [0].iter().cloned().collect();
+
+        assert_eq!(col_out, exp_col_out);
     }
 }
